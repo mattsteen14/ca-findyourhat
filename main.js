7ba3bdac2779749d3e63c8ece523ee2c9f7736f4
mattsteen14 <mattsteen14@me.com>
2024-05-28 13:35:31 +0100

    playChoice, resetField

--- main.js
+++ main.js
@@ -14,6 +14,25 @@ class Field {
     print() {
         this.field.forEach(row => console.log(row.join('')));
     }
+    playerChoice() {
+        const choice = prompt('Would you like to play again? y or n: ');
+        if (choice.toLowerCase() === 'y') {
+            this.resetField();
+        } else {
+            console.log('Thanks for playing!');
+            process.exit();
+        }
+    }
+    resetField() {
+        this.field = [
+            [pathCharacter, fieldCharacter, hole],
+            [fieldCharacter, hole, fieldCharacter],
+            [fieldCharacter, hat, fieldCharacter],
+        ];
+        this.playerPosition = { x: 0, y: 0 };
+        console.clear();
+        // this.print();
+    }
     movePlayer(direction) {
         let { x, y } = this.playerPosition;
         switch (direction) {
@@ -35,14 +54,17 @@ class Field {
         }
         if (x < 0 || y < 0 || x >= this.field.length || y >= this.field[0].length) {
             console.log('Out of Bounds!');
+            this.playerChoice();
             return;
         }
         const nextPosition = this.field[x][y];
         if (nextPosition === hole) {
             console.log('You fell into a hole!');
+            this.playerChoice();
             return;
         } else if (nextPosition === hat) {
             console.log('You found your hat!');
+            this.playerChoice();
             return;
         }
         this.field[this.playerPosition.x][this.playerPosition.y] = fieldCharacter;
@@ -56,6 +78,9 @@ const myField = new Field([
     [fieldCharacter, hole, fieldCharacter],
     [fieldCharacter, hat, fieldCharacter],
 ]);
+
+console.clear();
+myField.print();
 
 while (true) {
     const direction = prompt('Which way? (u = up, d= down, l = left, r = right): ');
