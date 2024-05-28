204b9059364441ade9e7b29da6ed8d301372e1bb
mattsteen14 <mattsteen14@me.com>
2024-05-28 12:03:24 +0100

    playerPosition

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const pathCharacter = '*';
 class Field {
     constructor(field) {
         this.field = field;
+        this.playerPosition = {x: 0, y:0};
+        this.field[0][0] = pathCharacter;
     }
     print() {
         this.field.forEach(row => console.log(row.join('')));
