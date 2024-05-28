940a2585bf1cf2fb282e2df165a825d1a58cc061
mattsteen14 <mattsteen14@me.com>
2024-05-28 12:17:24 +0100

    movePlayer

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
 const hat = '^';
 const hole = 'O';
@@ -8,11 +8,32 @@ const pathCharacter = '*';
 class Field {
     constructor(field) {
         this.field = field;
-        this.playerPosition = {x: 0, y:0};
+        this.playerPosition = { x: 0, y: 0 };
         this.field[0][0] = pathCharacter;
     }
     print() {
         this.field.forEach(row => console.log(row.join('')));
+    }
+    movePlayer(direction) {
+        let { x, y } = this.playerPosition;
+        switch (direction) {
+            case 'u': // Move up
+                x -= 1;
+                break;
+            case 'd': // Move down
+                x += 1;
+                break;
+            case 'l': // Move left
+                y -= 1;
+                break;
+            case 'r': // Move right
+                y += 1;
+                break;
+            default:
+                console.log('Invalid move');
+                return;
+        }
+        
     }
 }
 
